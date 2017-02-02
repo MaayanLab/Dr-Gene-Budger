7ef30e6f01b62e33df992160540027ee8e9913b7
Edward He <edward.he@macaulay.cuny.edu>
2017-02-02 11:48:39 -0500

    Styling buttons.

--- DGB/forms.py
+++ DGB/forms.py
@@ -1,15 +1,15 @@
 from flask_wtf import Form
-from wtforms import StringField, SubmitField, RadioField
+from wtforms import StringField, SubmitField, RadioField, SelectField
 from wtforms import validators  # ValidationError
 
 
 class GeneForm(Form):
     symbol = StringField("Gene Symbol", [validators.DataRequired("Please enter a gene symbol.")])
-    expression = RadioField("In which direction would you like to affect expression of the target gene?",
+    expression = SelectField("In which direction would you like to affect expression of the target gene?",
                             [validators.data_required("Please choose up or down")],
                             choices=[('Up', 'Up-Regulated'), ('Down', 'Down-Regulated')],
                             default='Up')
-    dataset = RadioField("Which datasets would you like to query?",
+    dataset = SelectField("Which datasets would you like to query?",
                          [validators.data_required("Please choose an option")],
                          choices=[('L1000', 'L1000'), ('CREEDS', 'CREEDS'), ('Both', 'Both')],
                          default='L1000')

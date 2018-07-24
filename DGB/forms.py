7aed71696137480695eb351cf618b6f3bd90e831
Zichen Wang <wangzc921@gmail.com>
2018-07-24 14:10:17 -0400

    Changed wording to include "small molecules"

--- DGB/forms.py
+++ DGB/forms.py
@@ -4,8 +4,8 @@ from wtforms import validators  # ValidationError
 
 class GeneForm(Form):
     symbol = StringField("Gene Symbol", [validators.DataRequired("Please enter a gene symbol.")])
-    expression = SelectField("In which direction would you like to affect expression of the target gene?",
-                            [validators.data_required("Please choose up or down")],
-                            choices=[('Up-Regulate', 'Up-Regulate'), ('Down-Regulate', 'Down-Regulate')],
-                            default='Up-Regulate')
+    # expression = SelectField("In which direction would you like to affect expression of the target gene?",
+    #                         [validators.data_required("Please choose up or down")],
+    #                         choices=[('Up-Regulate', 'Up-Regulate'), ('Down-Regulate', 'Down-Regulate')],
+    #                         default='Up-Regulate')
     submit = SubmitField("Find a list of prioritized small molecules")
